925b7332ddffaa760e585cfcfdb095dce478ae38
Kyle Acuna <134776227+giyugg@users.noreply.github.com>
2026-02-05 22:38:04 +0800

    Initial commit.

---
+++ src/Activity1_OddEvenNumbers/Main.java
@@ -0,0 +1,11 @@
+/* Objective:
+Write and design a program that asks for any number of inputs from the user.
+Then, your program should count the total number of ODD and EVEN numbers.
+Display the output and provide the sum of all inputted numbers. */
+package Activity1_OddEvenNumbers;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Activity2_PrepaidCellphoneSimulation/Main.java
@@ -0,0 +1,10 @@
+/* Objective:
+Design a functional program that implement a class that emulates a cellphone
+with a prepaid load and an initially charged battery. Consider the ff requirements and details:
+Initial load is PHP 15.00 ; Initial charged battery value of 10.
+Everytime that the cellphone sends an SMS message, the battery charged value decreases by 1, 2 every missed call
+and 3 every call. Max load is 15 and batt of 10. See notebook for more details. */
+package Activity2_PrepaidCellphoneSimulation;
+import java.util.Scanner;
+public class Main {
+}
