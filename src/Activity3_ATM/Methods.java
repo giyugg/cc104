604081610a7523e48cf6bb223f4641934a1500df
Kyle Acuna <134776227+giyugg@users.noreply.github.com>
2026-02-05 21:06:47 +0800

    exit logic is basic asf

--- src/Activity3_ATM/Methods.java
+++ src/Activity3_ATM/Methods.java
@@ -1,4 +1,9 @@
 package Activity3_ATM;
 
 public class Methods {
+
+    static void exitLogic() {
+        System.out.println("Thank you for banking with us! Goodbye.");
+        System.exit(0);
+    }
 }
