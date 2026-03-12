33cd70e88ad8998629f7a6ed0a5f75ae29aabb88
GiyuuuGG <134776227+giyugg@users.noreply.github.com>
2026-03-12 13:35:06 +0800

    Ask the user to input values for a 2×3 matrix, then print the matrix.

---
+++ src/Practice2DArray20260312/InputDisplayMatrixPractice.java
@@ -0,0 +1,17 @@
+package Practice2DArray20260312;
+import java.util.Scanner;
+// Ask the user to input values for a 2×3 matrix, then print the matrix.
+public class InputDisplayMatrixPractice {
+    static void main(String[] args) {
+        Scanner value = new Scanner(System.in);
+        int[][] matrix = new int[2][3];
+
+        System.out.println("Enter numbers: ");
+        for (int row = 0; row < matrix.length; row++) {
+            for (int column = 0; column < matrix[row].length; column++) {
+                matrix[row][column] = value.nextInt();
+                System.out.print(matrix[row][column] + " ");
+            } System.out.println();
+        }
+    }
+}
