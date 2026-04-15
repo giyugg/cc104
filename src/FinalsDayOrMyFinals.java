3d784b0888a09b756b198925d0505a6b53a4f40c
GiyuuuGG <134776227+giyugg@users.noreply.github.com>
2026-04-15 15:29:47 +0800

    1. Reverse String: now ok hehe

---
+++ src/FinalsDayOrMyFinals.java
@@ -0,0 +1,71 @@
+import java.util.Scanner;
+public class FinalsDayOrMyFinals {
+    static String sentence = "";
+
+    static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        System.out.print("Enter a string: ");
+        sentence = input.nextLine();
+
+        boolean terminate = false;
+        while (!terminate) {
+            displayMenu();
+            System.out.print("Enter your choice: ");
+            int choice = getIntegerInput();
+
+            switch (choice) {
+                case 1:
+                    reverseString(sentence);
+                    break;
+            }
+        }
+
+    }
+
+    public static void displayMenu() {
+        System.out.println("=================================");
+        System.out.println("   String Practice!!!");
+        System.out.println("   Your string is: " + sentence);
+        System.out.println("=================================");
+        System.out.println("1. Reverse String");
+        System.out.println("2. Remove Letters");
+        System.out.println("3. Remove Special Characters");
+        System.out.println("4. Remove Numbers");
+        System.out.println("5. Normalize Spacing each Word");
+        System.out.println("6. Extract Words");
+        System.out.println("7. Word Count");
+        System.out.println("8. Character Count");
+        System.out.println("9. Check if Palindrome");
+        System.out.println("10. Remove duplicated words");
+        System.out.println("11. Remove duplicated characters");
+        System.out.println("12. Character Compression");
+        System.out.println("13. Sort");
+        System.out.println("14. Alternate Two Strings");
+        System.out.println("15. Replace Substring");
+        System.out.println("16. Find Longest Word");
+        System.out.println("17. Find Shortest Word");
+    }
+
+    public static int getIntegerInput() {
+        while (!input.hasNextInt()) {
+            System.out.print("Invalid input. Please enter a number: ");
+            input.next();
+        }
+        int var = input.nextInt();
+        input.nextLine();
+        return var;
+    }
+
+    public static void reverseString(String sentence) {
+        System.out.println("=====================");
+        System.out.println("Status: It's working!");
+
+        String reversal = "";
+        for (int i = sentence.length() - 1; i >= 0; i--) {
+            reversal = reversal + sentence.charAt(i);
+        }
+
+        System.out.println("Reversed string: " + reversal);
+    }
+}
